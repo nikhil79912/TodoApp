3f4bcfe9ad34b0381df392d2f3d793b8ff6f3ff2
nikhil <nikhilgkp97@gmail.com>
2023-04-15 22:39:59 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
+  // const [error, setError] =useState(false);
+
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, []);
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>TODO App</h1>
+        <h2>Organize your work and life, finally.</h2>
+        <h4>
+          Become focused, organized, and calm with Todoist. The world’s #1 task
+          manager and to-do list app.
+        </h4>
+
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Add ToDos..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <div className="add">
+            <Button
+              variant="primary"
+              onClick={
+                isUpdating
+                  ? () =>
+                      updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+                  : () => addToDo(text, setText, setToDo)
+              }
+            >
+              {isUpdating ? "Update" : "Add"}
+            </Button>
+          </div>
+        </div>
+
+        <div className="list">
+          {toDo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
